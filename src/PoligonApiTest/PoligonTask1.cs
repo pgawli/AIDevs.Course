90f2c942e06c4cf05bfb190b76412cac405dfbb7
pgawli <piotr.gawlicki@xiron.pl>
2025-05-13 12:24:59 +0200

    Poligon Task1

---
+++ src/PoligonApiTest/PoligonTask1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Refit;
+
+namespace PoligonApiTest;
+
+public interface IPoligonApi
+{
+  [Get("/dane.txt")]
+  Task<string> GetPoligonData();
+  
+  [Post("/verify")]
+  Task<PoligonTask1Response> PostAnswer(PoligonTask1Request request);
+}
+
+public class PoligonTask1
+{
+  private readonly IPoligonApi poligonApi;
+  private readonly IConfiguration configuration;
+
+  public PoligonTask1(IPoligonApi poligonApi, IConfiguration configuration)
+  {
+    this.poligonApi = poligonApi;
+    this.configuration = configuration;
+  }
+
+  public async Task Execute()
+  {
+    var data = await poligonApi.GetPoligonData();
+    var apiKey = configuration["Api:Key"]!;
+    var tablica = data.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+    var request = new PoligonTask1Request("POLIGON", apiKey, tablica);
+    var response = await poligonApi.PostAnswer(request);
+    Console.WriteLine(response);
+  }
+}
+
+public record PoligonTask1Request(string Task, string Apikey, string[] Answer);
+public record PoligonTask1Response(int Code, string Message);
---
+++ src/PoligonApiTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PoligonApiTest;
+using Refit;
+
+var builder = new HostBuilder()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddUserSecrets<Program>(); // Dodaje sekcję secrets
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var apiUrl = context.Configuration["Api:BaseUrl"]!;
+        // var apiKey = context.Configuration["Api:Key"];
+
+        services.AddRefitClient<IPoligonApi>()
+            .ConfigureHttpClient(client =>
+        {
+            client.BaseAddress = new Uri(apiUrl);
+        });
+        services.AddTransient<PoligonTask1>();
+    });
+
+var app = builder.Build();
+await app.StartAsync();
+try
+{
+    var task1 = app.Services.GetRequiredService<PoligonTask1>();
+    await task1.Execute();
+}
+finally
+{
+    await app.StopAsync();
+};
