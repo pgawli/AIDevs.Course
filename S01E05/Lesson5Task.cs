49496dfa418f7eadc9cd38f3b75649d7b8905050
pgawli <piotr.gawlicki@xiron.pl>
2025-05-16 08:50:07 +0200

    Lekcja 5

---
+++ S01E05/Lesson5Task.cs
@@ -0,0 +1,76 @@
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
+using Refit;
+
+namespace S01E05;
+
+public class Lesson5Task
+{
+  private readonly ILesson5Api lesson5Api;
+  private readonly IConfiguration configuration;
+  private readonly ChatClient client;
+  private readonly string apiKey;  
+
+  public Lesson5Task(ILesson5Api lesson5Api, IConfiguration configuration)
+  {
+    this.lesson5Api = lesson5Api;
+    this.configuration = configuration;
+    client = new ChatClient(model: "gpt-4.1-nano", apiKey: configuration["OpenAI:Token"]!);
+    apiKey = configuration["ApiKey"]!;
+  }
+  public async Task Execute()
+  {
+    var cenzura = await lesson5Api.GetCenzuraTxt(apiKey);
+    if (!cenzura.IsSuccessful)
+    {
+      Console.WriteLine($"{cenzura.StatusCode} {cenzura.ReasonPhrase} - {cenzura.Content}");
+      throw new Exception("The cenzura.txt could not be retrieved.");
+    }
+    var originalText = cenzura.Content;
+    Console.WriteLine($"Cenzura.txt: {originalText}");
+    
+    var context = new List<ChatMessage>
+    {
+      ChatMessage.CreateSystemMessage("Imię i nazwisko muszą być zastąpione jednym słowem CENZURA" ),
+      ChatMessage.CreateSystemMessage("W adresie zastęp tylko nazwy miast i ulic ale zachowaj określenia miejsc takie jak 'ulica' i podobne" ),
+    };
+
+    var prompt = $"To jest tekst: '{originalText}' który należy ocenzurować. Zamień wszystkie dane dotyczące imienia, nazwiska, adresu oraz wieku na słowo CENZURA.";
+
+    context.Add(ChatMessage.CreateSystemMessage(prompt));
+    
+    var chatResponse = await client.CompleteChatAsync(context);
+    if (chatResponse == null)
+    {
+      throw new Exception("The chat response could not be retrieved.");
+    }
+    Console.WriteLine(chatResponse.Value.Content[0].Text);
+    
+    var answer = chatResponse.Value.Content[0].Text;
+    
+    var request = new AnswerRequest("CENZURA", apiKey, answer);
+    var response = await lesson5Api.PostCenzura(request);
+    if (!response.IsSuccessful)
+    {
+      Console.WriteLine($"{cenzura.StatusCode} {cenzura.ReasonPhrase} - {cenzura.Content}");
+      throw new Exception("The answer could not be posted.");
+    }
+    Console.WriteLine($"Answer: {response.Content}");
+  }
+  
+}
+
+public interface ILesson5Api
+{
+  [Get("/data/{apikey}/cenzura.txt")]
+  Task<ApiResponse<string>> GetCenzuraTxt(string apikey);
+  
+  [Post("/report")]
+  Task<ApiResponse<string>> PostCenzura(AnswerRequest request);
+}
+
+public record AnswerRequest(
+  [property: JsonPropertyName("task")] string Task, 
+  [property: JsonPropertyName("apikey")] string ApiKey, 
+  [property: JsonPropertyName("answer")] string Answer);
---
+++ S01E05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Refit;
+using S01E05;
+
+var builder = new HostBuilder()
+  .ConfigureAppConfiguration((hostingContext, config) =>
+  {
+    config.AddUserSecrets<Program>(); // Dodaje sekcję secrets
+  })
+  .ConfigureServices((context, services) =>
+  {
+    var apiUrl = context.Configuration["BaseUrl"]!;
+
+    var jsonSettings = new RefitSettings
+    {
+      ContentSerializer = new SystemTextJsonContentSerializer(
+        new JsonSerializerOptions
+        {
+          RespectNullableAnnotations = true,
+          PropertyNamingPolicy = null
+        }
+      )
+    };
+    
+    services.AddRefitClient<ILesson5Api>(jsonSettings)
+      .ConfigureHttpClient(client =>
+      {
+        client.BaseAddress = new Uri(apiUrl);
+      });
+    services.AddTransient<Lesson5Task>();
+  });
+
+var app = builder.Build();
+await app.StartAsync();
+try
+{
+  var lesson = app.Services.GetRequiredService<Lesson5Task>();
+  await lesson.Execute();
+}
+finally
+{
+  await app.StopAsync();
+};
